119d1b28ddc3a9c96ab11ac9c98ba8f066885b5a
mcarthur <mcarthur.alford@proton.me>
2024-11-18 02:04:59 +0000

    Spatial 2 Woo

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,76 @@ mod spatial;
 
 use crate::scheduler::TickratePlugin;
 use crate::scripting::ScriptingPlugin;
+use bevy::ecs::component::Tick;
+use bevy::math::{uvec2, vec2};
 use bevy::prelude::*;
+use bevy::render::render_resource::{
+    Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+};
+use bevy_fast_tilemap::bundle::MapBundleManaged;
+use bevy_fast_tilemap::map::Map;
+use bevy_fast_tilemap::plugin::FastTileMapPlugin;
+use spatial::apply_velocity;
 
 #[derive(Component)]
-struct Symbol {
-    char: char,
-    color: Color,
+struct Window {
+    width: i32,
+    height: i32,
+    depth: i32,
+    orientation: IVec3,
+}
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<Map>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let tiles_texture = asset_server.load("test_tiles.png");
+
+    let map = Map::builder(uvec2(1024, 1024), tiles_texture, vec2(16., 16.)).build_and_set(|_| 2);
+
+    commands.spawn(MapBundleManaged::new(map, materials.as_mut()));
+
+    // let size = Extent3d {
+    //     width: 512,
+    //     height: 512,
+    //     ..Default::default()
+    // }
+
+    // let mut image = Image {
+    //     texture_descriptor: TextureDescriptor {
+    //         label: None,
+    //         size: size,
+    //         dimension: TextureDimension::D2,
+    //         format: TextureFormat::Bgra8UnormSrgb,
+    //         mip_level_count: 1,
+    //         sample_count: 1,
+    //         usage: TextureUsages::TEXTURE_BINDING
+    //             | TextureUsages::COPY_DST
+    //             | TextureUsages::RENDER_ATTACHMENT,
+    //         view_formats: &[]
+    //     },
+    //     ..Default::default()
+    // };
+
+    // image.resize(size);
 }
 
 fn main() {
     App::new()
         .add_plugins((
-            MinimalPlugins,
-            bevy::log::LogPlugin {
+            DefaultPlugins.set(bevy::log::LogPlugin {
                 // level: bevy::log::Level::TRACE,
                 filter: "bevy_ecs=info,scheduler=warn".to_string(),
                 ..Default::default()
-            },
+            }),
+            FastTileMapPlugin::default(),
             ScriptingPlugin,
             TickratePlugin,
         ))
+        .add_systems(Startup, startup)
+        .add_systems(Tick, apply_velocity)
         .run();
 }
--- src/spatial.rs
+++ src/spatial.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
+#[derive(Resource)]
+struct Spatial {
+    size: IVec3,
+    entities: Vec<Entity>,
+}
+
+#[derive(Resource)]
+struct SpatialChanges(Vec<usize>);
+
 #[derive(Component)]
 struct Position {
     x: i64,
@@ -14,7 +23,7 @@ struct Velocity {
     z: i64,
 }
 
-fn apply_velocity(mut query: Query<(&Velocity, &mut Position)>) {
+pub fn apply_velocity(mut query: Query<(&Velocity, &mut Position)>) {
     // We need some universal resource for collision detecting that we can access
     // just a coordinate -> blocked/free map (maybe an entity id for whatever is in the cell)
     query.par_iter_mut().for_each(|(vel, mut pos)| {
@@ -22,4 +31,38 @@ fn apply_velocity(mut query: Query<(&Velocity, &mut Position)>) {
         pos.y += vel.y;
         pos.z += vel.z;
     });
+}
+
+impl Spatial {
+    fn index(&self, mut pos: IVec3) -> Option<usize> {
+        pos /= self.size;
+        let index = pos.x * (self.size.y * self.size.z) + pos.y * (self.size.z) + pos.z;
+        if index > self.size.y * self.size.z * self.size.x {
+            None
+        } else {
+            Some(index as usize)
+        }
+    }
+
+    fn spatial(&self, index: usize) -> Option<IVec3> {
+        let index = index as i32;
+        if index > self.size.y * self.size.z * self.size.x {
+            None
+        } else {
+            Some(IVec3::new(
+                index / (self.size.y * self.size.z),
+                (index % (self.size.y * self.size.z)) / self.size.z,
+                index % self.size.z,
+            ))
+        }
+    }
+
+    fn at(&self, pos: IVec3) -> Option<Entity> {
+        let index = self.index(pos)?;
+        Some(self.entities[index])
+    }
+
+    fn update(&self, pos: IVec3, entity: Entity) {
+        let index = self.index(pos);
+    }
 }
